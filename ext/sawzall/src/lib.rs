9bbd1abefae2da23c56b982b4d22ed52340cea48
David Cornu <me@davidcornu.com>
2025-04-19 13:21:54 -0400

    First draft

--- ext/sawzall/src/lib.rs
+++ ext/sawzall/src/lib.rs
@@ -1,12 +1,95 @@
-use magnus::{function, prelude::*, Error, Ruby};
-
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
-}
+use ego_tree::NodeId;
+use magnus::{function, method, prelude::*, Error, RArray, Ruby};
+use scraper::{ElementRef, Html, Selector};
+use std::sync::{Arc, Mutex};
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("Sawzall")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+    module.define_singleton_method("parse_fragment", function!(parse_fragment, 1))?;
+    module.define_singleton_method("parse_document", function!(parse_document, 1))?;
+
+    let document_class = module.define_class("Document", ruby.class_object())?;
+    document_class.define_method("select", method!(Document::select, 1))?;
+
+    let node_class = module.define_class("Node", ruby.class_object())?;
+    node_class.define_method("name", method!(Node::name, 0))?;
+    node_class.define_method("html", method!(Node::html, 0))?;
+    node_class.define_method("inner_html", method!(Node::inner_html, 0))?;
+    node_class.define_method("attr", method!(Node::attr, 1))?;
+
     Ok(())
+}
+
+fn parse_fragment(fragment: String) -> Document {
+    Document::new(Html::parse_fragment(&fragment))
+}
+
+fn parse_document(document: String) -> Document {
+    Document::new(Html::parse_document(&document))
+}
+
+#[derive(Clone)]
+#[magnus::wrap(class = "Sawzall::Document", free_immediately)]
+struct Document(Arc<Mutex<Html>>);
+
+impl Document {
+    fn new(html: Html) -> Self {
+        Self(Arc::new(Mutex::new(html)))
+    }
+
+    fn select(&self, selectors: String) -> Result<RArray, Error> {
+        let selector = Selector::parse(&selectors).unwrap();
+        let rarray = RArray::new();
+        let html = self.0.lock().unwrap();
+
+        for element_ref in html.select(&selector) {
+            let node = Node {
+                id: element_ref.id(),
+                document: self.clone(),
+            };
+            rarray.push(node)?;
+        }
+
+        Ok(rarray)
+    }
+}
+
+#[magnus::wrap(class = "Sawzall::Node", free_immediately)]
+struct Node {
+    id: NodeId,
+    document: Document,
+}
+
+impl Node {
+    fn map_element_ref<U, F>(&self, f: F) -> Option<U>
+    where
+        F: FnOnce(ElementRef) -> U,
+    {
+        let html = self.document.0.lock().unwrap();
+
+        html.tree.get(self.id).and_then(ElementRef::wrap).map(f)
+    }
+
+    fn name(&self) -> Option<String> {
+        self.map_element_ref(|element_ref| element_ref.value().name().to_string())
+    }
+
+    fn html(&self) -> Option<String> {
+        self.map_element_ref(|element_ref| element_ref.html())
+    }
+
+    fn inner_html(&self) -> Option<String> {
+        self.map_element_ref(|element_ref| element_ref.inner_html())
+    }
+
+    fn attr(&self, attribute: String) -> Option<String> {
+        self.map_element_ref(|element_ref| element_ref.attr(&attribute).map(ToString::to_string))
+            .flatten()
+    }
+
+    // select
+    // attr
+    // text
+    // children
 }
--- spec/sawzall_spec.rb
+++ spec/sawzall_spec.rb
@@ -5,7 +5,128 @@ RSpec.describe Sawzall do
     expect(Sawzall::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:sample_fragment) do
+    <<~HTML
+      <h1>Hello, world</h1>
+      <p>This is an HTML fragment</p>
+    HTML
+  end
+
+  let(:sample_document) do
+    <<~HTML
+      <!doctype html>
+      <html>
+        <head>
+          <title>Test Document</title>
+        </head>
+        <body>
+          <h1>Hello, world</h1>
+          <p>This is an HTML document</p>
+        </body>
+      </html>
+    HTML
+  end
+
+  describe ".parse_fragment" do
+    it "returns a Sawzall::Document" do
+      doc = Sawzall.parse_fragment(sample_fragment)
+
+      expect(doc).to be_a(Sawzall::Document)
+    end
+  end
+
+  describe ".parse_document" do
+    it "returns a Sawzall::Document" do
+      doc = Sawzall.parse_document(sample_document)
+
+      expect(doc).to be_a(Sawzall::Document)
+    end
+  end
+
+  describe Sawzall::Document do
+    describe "#select" do
+      it "returns nodes that match the CSS selector" do
+        doc = Sawzall.parse_document(sample_document)
+
+        selection = doc.select("p")
+        expect(selection.size).to eq(1)
+        expect(selection[0]).to be_a(Sawzall::Node)
+        expect(selection[0].name).to eq("p")
+        expect(selection[0].inner_html).to eq("This is an HTML document")
+      end
+
+      it "works with multiple matching elements" do
+        doc = Sawzall.parse_fragment(<<~HTML)
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+          </ul>
+        HTML
+
+        selection = doc.select("ul li")
+        expect(selection.map(&:name)).to eq(["li", "li"])
+        expect(selection.map(&:inner_html)).to eq(["One", "Two"])
+      end
+
+      it "returns nothing if there are no matching nodes" do
+        doc = Sawzall.parse_document(sample_document)
+
+        selection = doc.select("table")
+        expect(selection).to be_empty
+      end
+
+      skip "raises an error if the selector is invalid" do
+        doc = Sawzall.parse_fragment("")
+
+        expect { doc.select("][") }
+          .to raise_error("asdasda")
+      end
+    end
+  end
+
+  describe Sawzall::Node do
+    describe "#name" do
+      it "returns the element's name" do
+        doc = Sawzall.parse_fragment("<h1>Heading</h1>")
+
+        expect(doc.select("h1").first.name).to eq("h1")
+      end
+    end
+
+    describe "#html" do
+      it "returns the element's outer HTML" do
+        doc = Sawzall.parse_fragment("<h1>Heading</h1>")
+
+        expect(doc.select("h1").first.html).to eq("<h1>Heading</h1>")
+      end
+    end
+
+    describe "#inner_html" do
+      it "returns the element's inner HTML" do
+        doc = Sawzall.parse_fragment("<h1>Heading</h1>")
+
+        expect(doc.select("h1").first.inner_html).to eq("Heading")
+      end
+
+      it "returns an empty string if there is no inner HTML" do
+        doc = Sawzall.parse_fragment("<img src='https://example.com/image.png'/>")
+
+        expect(doc.select("img").first.inner_html).to eq("")
+      end
+    end
+
+    describe "#attr" do
+      it "returns the attribute value" do
+        doc = Sawzall.parse_fragment("<h1 id='main-heading'>Heading</h1>")
+
+        expect(doc.select("h1").first.attr("id")).to eq("main-heading")
+      end
+
+      it "returns nil when the attribute does not exist" do
+        doc = Sawzall.parse_fragment("<h1 id='main-heading'>Heading</h1>")
+
+        expect(doc.select("h1").first.attr("class")).to be_nil
+      end
+    end
   end
 end
